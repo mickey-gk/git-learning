5df33f6f4135be5a3b19d26871840ffab602ea9f
mickey-gk <mickeygkatana@gmail.com>
2024-06-04 21:24:50 +0300

    committing a python file to github

---
+++ simple-python-file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#this my first python file, created in vim, the basic editor for git bash.
+
+print("hello there!")
+print("welcome to my repository and git hub account")
+print("my name is MICHAEL KATANA")
